062a73096f574ca5a936ee8b6ee4cfcd0fd653a6
Piotr Rybinski <rybinski.p@icloud.com>
2020-11-02 00:32:13 +0100

    Initial

--- parser/wikiParser.py
+++ parser/wikiParser.py
@@ -5,17 +5,24 @@ class WikiParser(BaseParser):
         super().__init__(url)
 
     def getLinks(self, element, classname, needle, subelement, attribute):
+        print('starded')
         response = []
         elements = self.getElementByClass(element, classname)
         for idx, element in enumerate(elements):
+            print(idx)
             check = self.findInString(element, needle)
+            print(check)
             if check != -1:
                 subelements = self.getElement(element)
+                print(subelements)
                 subresponse = []
                 for subelement in subelements:
                     result = self.getAttribute(attribute)
+                    print(result)
                     subresponse.append(result)
-            response[idx].append(subresponse)
+                #response[idx].append(subresponse)
+                print(subresponse)
+                response.append(subresponse)
 
         return response
 
