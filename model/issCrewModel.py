ea11b062c9849fc18373f595b289760e27632792
Piotr Rybinski <rybinski.p@icloud.com>
2020-11-05 23:40:56 +0100

    Data matcher completed

--- model/issCrewModel.py
+++ model/issCrewModel.py
@@ -1,9 +1,10 @@
 import sys
 import os
-from decouple import config
 # Tell syspath where to import modules from other folders in root direcotry
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+from decouple import config
+import re
 from apidata.astroApireader import issDataReader
 from parserdata.wikiParser import WikiParser
 
@@ -27,31 +28,30 @@ class issCrew():
     def getCurrentCrew(self):
         """ Get ISS crew """
         crew = issDataReader(self.apiUrl)
-        return crew.getAstroData()
+        # Transform list to dictionary
+        return dict.fromkeys(crew.getAstroData(), '') 
         
     def matchCrewWithAstros(self):
         """ Match ISS crew members with country """
         astros = self.getAllAstros()
         crew = self.getCurrentCrew()
-        astrodetail = []
+        # Switch list to dictionary
         for astro in astros:
+            # Join astro array to string
+            astroString = ' '.join([str(elem) for elem in astro]) 
             for item in crew:
-                # split item, reverse array and get first element to extract surname
-                #print(item)
-                array = item.split()
-                array.sort(reverse=False)
-                print(array)
-                for subitem in array:
-                    #print(subitem)
-                    if subitem in astro:
-                        astrodetail.append(astro) 
-                        astrodetail.append(item)
-        return astrodetail
+                # Extract surname (last part of string after last whitespace)
+                surname = (item.split(' ')[-1]).strip()
+                # Check if surname exist in string
+                regmatch = re.search(surname, astroString)
+                if regmatch != None:
+                    crew[item] = astro
+        return crew
 
 x = issCrew()
 #astro = x.getAllAstros()
-crew = x.getCurrentCrew()
+#crew = x.getCurrentCrew()
 match = x.matchCrewWithAstros()
 #print(astro)
-print(crew)
+#print(crew)
 print(match)
