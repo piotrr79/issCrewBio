0272735cb6b84c3d3161367b84791faa56a940c6
Piotr Rybinski <rybinski.p@icloud.com>
2020-11-08 20:51:04 +0100

    Data matcher response fixed

--- model/issCrewModel.py
+++ model/issCrewModel.py
@@ -22,23 +22,6 @@ class issCrew():
         self.subelement = config('SUBELEMENT')
         self.attribute = config('ATTRIBUTE')
         self.apiUrl = config('API_URL')
-    
-    def getAllAstrosByName(self):
-        """ Get astronauts list from Wiki list by name """
-        parser = WikiParser(self.firstParserUrl )
-        return parser.getSubElementsByAttribute(self.element, self.classname, self.needle, self.subelement, self.attribute)
-
-    def getAllAstrosTravelers(self):
-        """ Get travelers list from Wiki travelers by name """
-        parser = WikiParser(self.secondParserUrl)
-        return parser.getSubElementsByAttribute(self.element, self.classname, self.needle, self.subelement, self.attribute)
-    
-    # Set up different parsing elements
-    # @ToDo - test parser with td elements on that 
-    def getAllAstrosByFirstFlight(self):
-        """ Get astronauts list from Wiki by first flight """
-        parser = WikiParser(self.thirdParserUrl)
-        return parser.getSubElementsByAttribute(self.element, self.classname, self.needle, self.subelement, self.attribute)
 
     def getAstrosParserData(self):
         """ Get astronauts list from Wiki list by name """
@@ -46,6 +29,7 @@ class issCrew():
         response = {}
         for idx, url in enumerate(urls):
             parser = WikiParser(url)
+            # @ToDo - extend iteration to accept results from different parsers (eg parsing by td not li)
             subresponse = parser.getSubElementsByAttribute(self.element, self.classname, self.needle, self.subelement, self.attribute)
             response[idx] = subresponse
         return response
@@ -59,39 +43,27 @@ class issCrew():
     def matchCrewWithAstros(self):
         """ Match ISS crew members with country and otjer available data """
         astros = self.getAstrosParserData()      
-        crew = self.getCurrentCrew() 
+        crew = self.getCurrentCrew()    
+        response = {} 
         for key, astroArray in astros.items():
-            # Join astro array to string
             for astroItem in astroArray:
-                """ Create strinf from element """
-                astroString = ' '.join([str(elem) for elem in astroItem]) 
+                # Join astro array to string
+                astroString = ' '.join([str(elem) for elem in astroItem])           
                 for item in crew:
                     # Extract surname (last part of string after last whitespace)
-                    surname = (item.split(' ')[-1]).strip()
+                    surname = (item.split(' ')[-1]).strip()                   
                     # Check if surname exist in string
                     regmatch = re.search(surname, astroString)
                     if regmatch != None:
-                        crew[item] = {key: astroItem}
-                        '''if crew[item] is None:
-                            crew[item] = astroItem
-                        else:
-                            oldVal = crew[item]
-                            print(oldVal)
-                            print(astroItem)
-                            #crew[item].append(astroItem)
-                            crew[item] = oldVal + astroItem'''
-            return crew
+                       crew[item] = astroItem
+            # Push response (crew) from each iteration to final repsonse with key
+            response[key] = crew
+        return response
 
 x = issCrew()
-#astro1 = x.getAllAstrosByName()
-#astro2 = x.getAllAstrosTravelers()
-#astro3 = x.getAllAstrosByFirstFlight()
-#astro4 = x.getAstrosParserData()
+#astro = x.getAstrosParserData()
 #crew = x.getCurrentCrew()
 match = x.matchCrewWithAstros()
-#print(astro1)
-#print(astro2)
-#print(astro3)
-#print(astro4)
+#print(astro)
 #print(crew)
 print(match)
